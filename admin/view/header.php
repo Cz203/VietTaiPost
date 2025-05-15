b95fb33a3bcf397e81ced4c9d5141a4fee143642
BuiAnhTai_21020981 <buianhtai4903@gmail.com>
2025-05-15 23:26:53 +0700

    add trang admin

---
+++ admin/view/header.php
@@ -0,0 +1,9 @@
+<div class="px-3 py-2 border-bottom mb-3 shadow-sm bg-white">
+      <div class="container d-flex flex-wrap justify-content-center">
+        <div class="text-end">
+          Xin chào Admin
+          <button type="button" class="btn btn-light text-dark me-2">Đăng nhập</button>
+          <button type="button" class="btn btn-primary">Đăng xuất</button>
+        </div>
+      </div>
+    </div>
---
+++ admin/view/sidebar.php
@@ -0,0 +1,35 @@
+<div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 280px;">
+    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      <span class="fs-4">VIETTAI-POST</span>
+    </a>
+    <hr>
+    <ul class="nav nav-pills flex-column mb-auto">
+      <li class="nav-item">
+        <a href="home.php" class="nav-link text-white" aria-current="page">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"></use></svg>
+          Trang chủ
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
+          Quản lí đơn hàng
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"></use></svg>
+          Quản lí nhân viên
+        </a>
+      </li>
+      <li>
+        <a href="#" class="nav-link text-white">
+          <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"></use></svg>
+          Quản lí bưu cục
+        </a>
+      </li>
+    </ul>
+    <hr>
+    
+  </div>
